994a71e642a33c386d381b026e5f9ddd791d2fe8
yuri4029 <tranminh1831996@gmail.com>
2021-07-08 22:28:56 +0900

    template for cp

---
+++ temp.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define MAXN 100005
+#define INF 1000000000000000005
+#define mod 1000000007
+#define rep(i,n) for (int i=0;i<n;i++)
+#define ll long long
+using namespace std;
+
+int dx[] ={ 0, 1, 0, -1}; // left, up, right, down
+int dy[] ={-1, 0, 1,  0};
+
+int dxx[] ={-1,-1, 1,  1}; // up left, down left, down right,  up right
+int dyy[] ={-1, 1, 1, -1};
+
+int main (){
+	
+		
+	
+	return 0;
+} 
