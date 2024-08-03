b046d50a91b7bf9aa39a1467b2bb694b56f31db5
yuri4029 <tranminh1831996@gmail.com>
2024-08-04 08:49:54 +0900

    Update cp temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,20 +1,47 @@
-#include <bits/stdc++.h>
-#define MAXN 100005
+#include <map>
+#define MAXN 200200
 #define INF 1000000000000000005
-#define mod 1000000007
-#define rep(i,n) for (int i=0;i<n;i++)
+#define mod 998244353
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep1(i, n) for (int i = 1; i <= n; i++)
 #define ll long long
+#define debug(x)        \
+    cout << #x << ": "; \
+    _print(x);          \
+    cout << endl;
+
 using namespace std;
 
-int dx[] ={ 0, 1, 0, -1}; // left, up, right, down
-int dy[] ={-1, 0, 1,  0};
+int dx[] = {0, 1, 0, -1}; // left, up, right, down
+int dy[] = {-1, 0, 1, 0};
 
-int dxx[] ={-1,-1, 1,  1}; // up left, down left, down right,  up right
-int dyy[] ={-1, 1, 1, -1};
+int dxx[] = {-1, -1, 1, 1}; // up left, down left, down right,  up right
+int dyy[] = {-1, 1, 1, -1};
+
+template <class T>
+void _print(vector<int> v1)
+{
+    for (int i : v1)
+        cout << i << " ";
+}
+void _print(long long a) { cout << a; }
+void _print(char a) { cout << a; }
+void _print(string a) { cout << a; }
+void _print(bool a) { cout << a; }
+
+typedef pair<int, int> pair_ii;
+
+void solve(){
+	
+}
 
 int main (){
 	
-		
-	
+	int t;
+	cin >> t;
+	while (t--) {
+         cout << endl; 
+	}
+	solve();
 	return 0;
 } 
